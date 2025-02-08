3731265097c3bc54ae716febf656abef3cc99b62
Darith <eledarith@gmail.com>
2025-02-09 01:26:04 +0700

    modify href navbar

--- app.js
+++ app.js
@@ -1,30 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const links = document.querySelectorAll("nav a");
-  const contentDiv = document.getElementById("content");
-
-  // Load default page (Home)
-  loadPage("home");
-
-  links.forEach((link) => {
-    link.addEventListener("click", (e) => {
-      e.preventDefault();
-      const page = e.target.getAttribute("data-page");
-      loadPage(page);
-    });
-  });
-
-  function loadPage(page) {
-    fetch(`pages/${page}.html`)
-      .then((response) => {
-        if (!response.ok) throw new Error("Page not found");
-        return response.text();
-      })
-      .then((data) => {
-        contentDiv.innerHTML = data;
-    })
-  }
-});
-
 const toggleBtn = document.getElementById("toggle-btn");
 
 toggleBtn.addEventListener("click", () => {
